5b1ea860bac8b032a97eb949f26819fc8c3ede25
Matthias <matthias.ambroise@gmail.com>
2018-12-11 19:10:19 +0100

    Petite Modif

--- src/main/java/Controller/SalesByState.java
+++ src/main/java/Controller/SalesByState.java
@@ -28,7 +28,7 @@ public class SalesByState extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
+     * methods. 
      *
      * @param request servlet request
      * @param response servlet response
