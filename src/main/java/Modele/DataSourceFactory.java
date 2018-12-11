eeb972d199a11644b00b7f1d43944434e79e6dcf
Matthias <matthias.ambroise@gmail.com>
2018-12-11 19:04:10 +0100

    Petite Modif

--- src/main/java/Modele/DataSourceFactory.java
+++ src/main/java/Modele/DataSourceFactory.java
@@ -5,25 +5,42 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.sql.DataSource;
 
-
 public class DataSourceFactory {
-     
-    static {
-        try {
-            Class.forName("org.apache.derby.jdbc.ClientDriver");
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(DataSourceFactory.class.getName()).log(Level.SEVERE, null, ex);
+
+    private enum DriverType {
+        embedded, server
+    };
+
+    private static final DriverType TYPE = DriverType.server;
+
+    /**
+     * Renvoie la source de données (server ou embbeded)
+     *
+     * @return la source de données
+     */
+    public static DataSource getDataSource() {
+        DataSource result;
+
+        switch (TYPE) {
+            case server: // Derby mode serveur, doit être démarré indépendamment
+                org.apache.derby.jdbc.ClientDataSource ds = new org.apache.derby.jdbc.ClientDataSource();
+                ds.setDatabaseName("sample");
+                ds.setUser("app");
+                ds.setPassword("app");
+                // The host on which Network Server is running
+                ds.setServerName("localhost");
+                // port on which Network Server is listening
+                ds.setPortNumber(1527);
+                result = ds;
+                break;
+            default: // Derby mode embedded, démarré automatiquement avec l'application
+                org.apache.derby.jdbc.EmbeddedDataSource es = new org.apache.derby.jdbc.EmbeddedDataSource();
+                es.setCreateDatabase("create");
+                es.setDatabaseName("embedded_sample");
+                result = es;
         }
+
+        return result;
     }
-    
-    public static DataSource getDataSource() throws SQLException {
-	org.apache.derby.jdbc.ClientDataSource ds = new org.apache.derby.jdbc.ClientDataSource();
-	ds.setDatabaseName("sample");
-	ds.setUser("app");
-	ds.setPassword("app");
-        ds.setServerName("localhost");
-        ds.setPortNumber(1527);
-        return ds;
-    }
-    
+
 }
